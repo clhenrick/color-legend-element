a060ee599db7b747863a31b4840be1e8ddad3eef
Chris Henrick <chrishenrick@gmail.com>
2021-11-28 18:15:48 -0800

    added layout properties and updated template

--- src/color-legend-element.ts
+++ src/color-legend-element.ts
@@ -1,18 +1,81 @@
-import {LitElement, html} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
+import { LitElement, html } from "lit";
+import { customElement, property, query } from "lit/decorators.js";
 
-@customElement('color-legend-element')
+import {
+  DEFAULT_WIDTH,
+  DEFAULT_HEIGHT,
+  DEFAULT_MARGIN_BOTTOM,
+  DEFAULT_MARGIN_LEFT,
+  DEFAULT_MARGIN_RIGHT,
+  DEFAULT_MARGIN_TOP,
+  DEFAULT_TITLE_TEXT,
+} from "./constants";
+
+@customElement("color-legend-element")
 export class ColorLegendElement extends LitElement {
+  /**
+   * The title text that displays at the top of the legend
+   */
   @property({ type: String })
-  name = 'World'
+  titleText = DEFAULT_TITLE_TEXT;
+
+  /**
+   * The width of the SVG or categorical legend div element
+   */
+  @property({ type: Number })
+  width = DEFAULT_WIDTH;
+
+  /**
+   * The height of the SVG element
+   */
+  @property({ type: Number })
+  height = DEFAULT_HEIGHT;
+
+  /**
+   * The spacing between the legend bar and top most extent of the SVG
+   */
+  @property({ type: Number })
+  marginTop = DEFAULT_MARGIN_TOP;
+
+  /**
+   * The spacing between the legend bar and right most extent of the SVG
+   */
+  @property({ type: Number })
+  marginRight = DEFAULT_MARGIN_RIGHT;
+
+  /**
+   * The spacing between the legend bar and bottom most extent of the SVG
+   */
+  @property({ type: Number })
+  marginBottom = DEFAULT_MARGIN_BOTTOM;
+
+  /**
+   * The spacing between the legend bar and left most extent of the SVG
+   */
+  @property({ type: Number })
+  marginLeft = DEFAULT_MARGIN_LEFT;
+
+  /**
+   * Reference to the SVG node
+   */
+  @query("svg")
+  svg!: SVGSVGElement;
 
   override render() {
-    return html`<p>Hello ${this.name}!</p>`;
+    const title = this.titleText ? html`<p class="legend-title">${this.titleText}</p>` : '';
+    
+    return html`<div
+      class="cle-container"
+      style="width:${this.width}px; height:auto;"
+    >
+      ${title}
+      <svg width=${this.width} height=${this.height}></svg>
+    </div>`;
   }
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    'color-legend-element': ColorLegendElement;
+    "color-legend-element": ColorLegendElement;
   }
 }
---
+++ src/constants.ts
@@ -0,0 +1,17 @@
+export const DEFAULT_WIDTH = 325;
+export const DEFAULT_HEIGHT = 32;
+
+export const DEFAULT_MARGIN_TOP = 6;
+export const DEFAULT_MARGIN_RIGHT = 0;
+export const DEFAULT_MARGIN_BOTTOM = 16;
+export const DEFAULT_MARGIN_LEFT = 0;
+
+export const DEFAULT_TICKS = DEFAULT_WIDTH / 64;
+export const DEFAULT_TICK_SIZE = 6;
+export const DEFAULT_TICK_VALUES: number[] | null = null;
+export const DEFAULT_TICK_FORMAT = ".1f";
+
+export const DEFAULT_DOMAIN = [0, 1];
+export const DEFAULT_RANGE = ["#ffffcc","#a1dab4","#41b6c4","#2c7fb8","#253494"]; // d3.schemeYlGnBu[5]
+
+export const DEFAULT_TITLE_TEXT = "Legend Title";
