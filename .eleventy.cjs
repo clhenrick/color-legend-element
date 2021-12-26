9ce209c1cf741cefb168b14da9ad620fea1afa98
Chris Henrick <chrishenrick@gmail.com>
2021-12-25 16:35:04 -0800

    use prismjs css from node_modules

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -20,6 +20,7 @@ module.exports = function (eleventyConfig) {
   );
   eleventyConfig.addPassthroughCopy("node_modules/lit/polyfill-support.js");
   eleventyConfig.addPassthroughCopy("node_modules/d3/dist/d3.min.js");
+  eleventyConfig.addPassthroughCopy("node_modules/prismjs/themes/prism-okaidia.css");
   eleventyConfig.addPassthroughCopy("build/color-legend-element.umd.js");
 
   eleventyConfig.addWatchTarget("./docs-src/assets");
